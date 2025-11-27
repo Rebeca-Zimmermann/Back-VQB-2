88b2f2926e82c5aeee4b0a876a95dbc4ab090c03
Rebeca-Zimmermann <rebpzim@gmail.com>
2025-11-27 08:21:43 -0300

    atualizando contato

--- app.js
+++ app.js
@@ -268,7 +268,7 @@ app.get("/locais/usuario/:id_usuario", async (req, res) => {
 //ROTA PUT - EDIÇÃO DOS LOCAIS ✅
 app.put("/locais/:id_local", async (req, res) => {
   const { id_local } = req.params;
-  const { id_usuario, nome_local, descricao, categoria, imagem } = req.body;
+  const { id_usuario, nome_local, descricao, categoria, imagem, contato } = req.body;
 
   try {
     // Busca função do usuário e o dono do local
@@ -292,7 +292,8 @@ app.put("/locais/:id_local", async (req, res) => {
         nome_local = ${nome_local}, 
         descricao = ${descricao}, 
         categoria = ${categoria}, 
-        imagem = ${imagem}
+        imagem = ${imagem},
+        contato = ${contato}
       WHERE id_local = ${id_local}
     `;
 
